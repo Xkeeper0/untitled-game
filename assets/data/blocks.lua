00413115850f04dbaf04389a4b9d4c07da65dc97
Xkeeper <xkeeper@gmail.com>
2016-08-27 22:35:36 -0700

    Add some descriptive comments

--- assets/data/blocks.lua
+++ assets/data/blocks.lua
@@ -5,8 +5,14 @@ BlockTranslationTable	= {
 	['H']		= 2
 	}
 
--- Block collision data.
--- ImageLocation is done in loading step
+-- Block appearance and collision data.
+-- imageLocation is the location of the tile:
+-- tileset = assets/images/??????.png
+-- x, y = 16x16-offset of the tile; so one right and down,
+-- (16,16)-(31,31) would just be "1,1"
+-- "type" is a little nebulous right now but "solid", "ladder", "empty"
+-- mean basically what you would expect.
+-- @todo In $future these will be procedurally fixed up for corners, etc. maybe
 BlockTypes		= {
 	[0]	= { imageLocation = { tileset = "tileset", x = "0", y = "0" }, type = "empty" },
 	[1]	= { imageLocation = { tileset = "tileset", x = "1", y = "0" }, type = "solid" },
